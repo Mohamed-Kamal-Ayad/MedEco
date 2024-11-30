dd8f7f382ed82bb56bc2dcff79e5869dc5867881
haweil <92003311+haweil@users.noreply.github.com>
2024-11-30 20:26:43 +0200

    edit drug pharmacy

--- app/Http/Resources/Pharmacy/DrugResource.php
+++ app/Http/Resources/Pharmacy/DrugResource.php
@@ -17,29 +17,12 @@ class DrugResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'description' => $this->description,
+            'points' => $this->points,
             'drug_type' => [
                 'id' => $this->drugType->id,
                 'name' => $this->drugType->name,
                 'unit' => $this->drugType->unit,
             ],
-            'user' => [
-                'id' => $this->user->id,
-                'name' => $this->user->name,
-                'email' => $this->user->email,
-                'phone' => $this->user->phone,
-            ],
-            'price' => $this->price,
-            'qty' => $this->qty,
-            'production_date' => $this->production_date,
-            'expiry_date' => $this->expiry_date,
-            'lat' => $this->lat,
-            'lng' => $this->lng,
-            'is_donated' => $this->is_donated,
-            'images' => $this->getMedia('images')->map(function ($image) {
-                return $image->getFullUrl();
-            }),
-            'created_at' => $this->created_at,
         ];
     }
 }
