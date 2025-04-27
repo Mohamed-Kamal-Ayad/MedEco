12c20b6775522e422ddd002f2f46c8247b353f95
Mohamed Kamal <mohamedkamalayad20@gmail.com>
2025-04-28 01:23:02 +0300

    Remove is_approved field from redeem creation in RedeemController

--- app/Http/Controllers/Api/User/RedeemController.php
+++ app/Http/Controllers/Api/User/RedeemController.php
@@ -47,7 +47,6 @@ class RedeemController extends Controller
             'user_id' => $user->id,
             'pharmacy_id' => $request->pharmacy_id,
             'points' => $request->points,
-            'is_approved' => false,
         ]);
         dd($redeem);
         return new RedeemResource($redeem);
