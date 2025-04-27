917831a29c4a31d1c35f1668615acd8d799d4d51
Mohamed Kamal <mohamedkamalayad20@gmail.com>
2025-04-28 01:09:25 +0300

    Debug user points retrieval in store method of RedeemController

--- app/Http/Controllers/Api/User/RedeemController.php
+++ app/Http/Controllers/Api/User/RedeemController.php
@@ -29,6 +29,7 @@ class RedeemController extends Controller
     public function store(Request $request)
     {
         $user = $request->user();
+        dd($user->points);
 
         $request->validate([
             'pharmacy_id' => 'required|exists:pharmacies,id',
